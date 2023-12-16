2bbbda145761d50a01cb023fdcc94d6aa378bdef
masud-rahman-mvn <masudur.rahman@moveon.com.bd>
2023-12-17 02:13:25 +0600

    done: create templatesp

--- buffer_cafe/meal/views.py
+++ buffer_cafe/meal/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+import requests
 
-# Create your views here.
+def fetch_data_from_url(url):
+    try:
+        response = requests.get(url)
+      
+        # Check if the request was successful (status code 200)
+        if response.status_code == 200:
+            # Parse the JSON content from the response
+            data = response.json()
+            return data
+        else:
+            print(f"Error: Unable to fetch data. Status code: {response.status_code}")
+            return None
+    except requests.RequestException as e:
+        print(f"Error: {e}")
+        return None
+
+def home(request):
+    json_url = "https://raw.githubusercontent.com/abdullahallnaim/batch3-week3-assignment3/main/meal.json"
+
+    # Fetch data from the URL
+    meal_data = fetch_data_from_url(json_url)
+
+    # Check if data was successfully fetched
+    if meal_data is not None:
+
+        return render(request, 'base.html', context={'data': 'meal_data'})
+    else:
+        print("Failed to fetch data.")
+        return render(request, 'base.html', context={'data': 'meal_data'})
